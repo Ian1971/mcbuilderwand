6cdc044787fd1ae965e34804d04a759aa0ee1cd8
Ian1971 <ian1971@gmail.com>
2022-05-22 20:49:59 +0100

    add hemisphere

--- src/scripts/actionlist.ts
+++ src/scripts/actionlist.ts
@@ -3,7 +3,7 @@ import { Sphere } from "./drawing/sphere";
 import { Cuboid } from "./drawing/cuboid";
 import { HollowCuboid } from "./drawing/hollow_cuboid";
 import { Pyramid } from "./drawing/pyramid";
-
+import { Hemisphere } from "./drawing/hemisphere";
  
 export const cancel:Action = new Action("cancel");
 export const undo:Action = new Action("undo");
@@ -11,7 +11,7 @@ const cuboidAction:Action = new Cuboid();
 const hollowCuboidAction:Action = new HollowCuboid();
 const pyramidAction:Action = new Pyramid();
 const sphereAction:Action = new Sphere();
-const hemisphereAction:Action = new Action("hemisphere", false);
+const hemisphereAction:Action = new Hemisphere();
 const coneAction:Action = new Action("cone");
 const lineAction:Action = new Action("line");
 const wallAction:Action = new Action("wall", false);
---
+++ src/scripts/drawing/hemisphere.ts
@@ -0,0 +1,32 @@
+import { BlockLocation, Player } from "mojang-minecraft";
+import * as vector from "./vector";
+import {Action, PlayerWandState} from "./../action"
+import * as logging from "./../logging"
+import { MapWithOffset} from "./vector";
+import { hemisphere_of_radius } from "./sphere";
+
+export class Hemisphere extends Action {
+
+    constructor()
+    {
+    super("hemi-sphere", false, true, true);
+    }
+    
+    execute(wandState: PlayerWandState):MapWithOffset | null 
+    {
+        const dir = vector.vectorAToB(wandState.firstPosition, wandState.secondPosition);
+        const radius = Math.round(vector.magnitude(dir));
+    
+        let map = hemisphere_of_radius(radius);
+
+        return map;
+    }
+
+    message(wandState: PlayerWandState){
+        logging.log(`You have chosen to create a hemi-sphere using ${wandState.firstBlock}`);
+        logging.log(`The first click choose the center.`);
+        logging.log(`The second click defines the radius.`);
+    }
+}
+
+ 
